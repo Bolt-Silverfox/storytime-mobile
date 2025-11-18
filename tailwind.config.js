def1367b79ee7c5d605ba19811f9552ad5b1a69b
Moluno-xiii <molunoprogress@gmail.com>
2025-11-18 13:50:48 +0100

    chore(nativewind) : install and configure nativewind + lucide-react

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./App.tsx",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./screens/**/*.{js,jsx,ts,tsx}",
+  ],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#EC4007",
+        "primary-light": "#FFC7CC",
+        border: "#4A413F",
+        text: "#616161",
+        link: "#0731EC",
+        black: "#212121",
+        "bg-light": "#fbf8f7",
+        disabled: "#FF8771",
+      },
+    },
+  },
+  plugins: [],
+};
