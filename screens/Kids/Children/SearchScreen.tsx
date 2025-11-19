c161a81b0326491440f360aae774644db5758f7b
Kelvin Achi <122989523+h3ktorr@users.noreply.github.com>
2025-11-19 21:52:52 +0100

    Replace SearchScreen content with ComingSoon component

--- screens/Kids/Children/SearchScreen.tsx
+++ screens/Kids/Children/SearchScreen.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import ComingSoon from "../../../components/ComingSoon";
 
 function SearchScreen() {
-  return (
-    <div>SearchScreen</div>
-  )
+  return <ComingSoon title="SearchScreen" />;
 }
+
 
 export default SearchScreen
