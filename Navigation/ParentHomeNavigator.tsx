bf49748e5b2cfc3fc8fdc81cda4b1dafcd4dc68f
Moluno-xiii <molunoprogress@gmail.com>
2025-11-19 20:50:38 +0100

    fix : bottom tab home navigator - added proper navigation nesting

---
+++ Navigation/ParentHomeNavigator.tsx
@@ -0,0 +1,44 @@
+import {
+  createNativeStackNavigator,
+  NativeStackNavigationProp,
+} from "@react-navigation/native-stack";
+import ChildStoryDetails from "../screens/parents/ChildStoryDetails";
+import ChallengeTracker from "../screens/parents/home/ChallengeTracker";
+import DailyChallengeScreen from "../screens/parents/home/DailyChallengeScreen";
+import InteractiveStoriesScreen from "../screens/parents/home/InteractiveStoriesScreen";
+import PlainStoriesScreen from "../screens/parents/home/PlainStoriesScreen";
+import StoriesListScreen from "../screens/parents/StoriesListScreen";
+
+type ParentHomeNavigatorParamList = {
+  childStoryDetails: { storyId: string };
+  storiesList: undefined;
+  interactiveStories: undefined;
+  plainStories: undefined;
+  dailyChallenge: undefined;
+  challengeTracker: undefined;
+  index: undefined;
+};
+
+type ParntHomeNavigatorProp =
+  NativeStackNavigationProp<ParentHomeNavigatorParamList>;
+const Stack = createNativeStackNavigator<ParentHomeNavigatorParamList>();
+
+const ParentHomeNavigator = () => {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" component={ParentHomeNavigator} />
+      <Stack.Screen name="storiesList" component={StoriesListScreen} />
+      <Stack.Screen name="childStoryDetails" component={ChildStoryDetails} />
+      <Stack.Screen
+        name="interactiveStories"
+        component={InteractiveStoriesScreen}
+      />
+      <Stack.Screen name="plainStories" component={PlainStoriesScreen} />
+      <Stack.Screen name="dailyChallenge" component={DailyChallengeScreen} />
+      <Stack.Screen name="challengeTracker" component={ChallengeTracker} />
+    </Stack.Navigator>
+  );
+};
+
+export type { ParntHomeNavigatorProp };
+export default ParentHomeNavigator;
---
+++ Navigation/ParentsReportNavigator.tsx
@@ -0,0 +1,27 @@
+import {
+  createNativeStackNavigator,
+  NativeStackNavigationProp,
+} from "@react-navigation/native-stack";
+import ReportDetailsScreen from "../screens/parents/reports/ReportDetailsScreen";
+import ReportsSreen from "../screens/parents/reports/ReportsSreen";
+
+type ParentReportNavigatorParamList = {
+  home: undefined;
+  reportDetails: undefined;
+};
+
+type ParntReportNavigatorProp =
+  NativeStackNavigationProp<ParentReportNavigatorParamList>;
+const Stack = createNativeStackNavigator<ParentReportNavigatorParamList>();
+
+const ParentReportNavigator = () => {
+  return (
+    <Stack.Navigator screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="home" component={ReportsSreen} />
+      <Stack.Screen name="reportDetails" component={ReportDetailsScreen} />
+    </Stack.Navigator>
+  );
+};
+
+export type { ParntReportNavigatorProp };
+export default ParentReportNavigator;
