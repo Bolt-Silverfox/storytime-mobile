7d1e587616307a12fd4ce4aef88eaeb50c6bdbc1
Onyinye Stephanie Oguocha <stephanieoguocha@gmail.com>
2025-11-19 21:42:58 +0100

    Add placeholder files for children category

---
+++ screens/Kids/Children/CharacterCustomization.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function CharacterCustomization() {
+  return (
+    <div>CharacterCustomization</div>
+  )
+}
+
+export default CharacterCustomization
---
+++ screens/Kids/Children/ChildProfile.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ChildProfile() {
+  return (
+    <div>ChildProfile</div>
+  )
+}
+
+export default ChildProfile
