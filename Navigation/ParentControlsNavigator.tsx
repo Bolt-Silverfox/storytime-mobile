bd5b4d5aa8b1fc95a59e513f8ce8676a421c5466
jutin0852 <jutindikonu8@gmail.com>
2025-11-20 23:54:46 +0100

    fix: moving changes to new branch

--- Navigation/ParentControlsNavigator.tsx
+++ Navigation/ParentControlsNavigator.tsx
@@ -4,10 +4,14 @@ import {
 } from "@react-navigation/native-stack";
 import LinkChild from "../screens/parents/profile/LinkChild";
 import ManageChild from "../screens/parents/profile/ManageChild";
+import EditChild from "../screens/parents/profile/EditChild";
+import DeleteSuccess from "../screens/parents/profile/DeleteSuccess";
 
 type ParentControlNavigatorParamList = {
   manageChild: undefined;
   linkChild: { childId: string };
+  editChild: undefined;
+  deleteSuccess: undefined;
 };
 
 type ParntControlNavigatorProp =
@@ -19,6 +23,8 @@ const ParentControlNavigator = () => {
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       <Stack.Screen name="manageChild" component={ManageChild} />
       <Stack.Screen name="linkChild" component={LinkChild} />
+      <Stack.Screen name="editChild" component={EditChild} />
+      <Stack.Screen name="deleteSuccess" component={DeleteSuccess} />
     </Stack.Navigator>
   );
 };
