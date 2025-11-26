383bdb2552db1926462e17dd129ff012a69f8515
Moluno-xiii <molunoprogress@gmail.com>
2025-11-26 15:21:48 +0100

    added update profile endpoint

--- screens/parents/controls/SetBedtime.tsx
+++ screens/parents/controls/SetBedtime.tsx
@@ -1,21 +1,37 @@
-import { useNavigation } from "@react-navigation/native";
+import { RouteProp, useNavigation, useRoute } from "@react-navigation/native";
+import { View, Text, ScrollView, Switch, Pressable } from "react-native";
 import {
-  StyleSheet,
-  View,
-  Text,
-  ScrollView,
-  Switch,
-  Pressable,
-} from "react-native";
-import { ParentControlNavigatorProp } from "../../../Navigation/ParentControlsNavigator";
+  ParentControlNavigatorParamList,
+  ParentControlNavigatorProp,
+} from "../../../Navigation/ParentControlsNavigator";
 import PageTitle from "../../../components/PageTitle";
 import { useState } from "react";
 import Icon from "../../../components/Icon";
 import TimePickerOverlay from "../../../components/modals/TimePickerOverlay";
+import useUpdateKids from "../../../hooks/tanstack/mutationHooks/useUpdateKids";
+import ErrorMessageDisplay from "../../../components/ErrorMessageDisplay";
+import LoadingOverlay from "../../../components/LoadingOverlay";
+import CustomButton from "../../../components/UI/CustomButton";
+import useGetKidById from "../../../hooks/tanstack/queryHooks/useGetKidById";
+import ErrorComponent from "../../../components/ErrorComponent";
+
+type PropRoutes = RouteProp<ParentControlNavigatorParamList, "setBedtime">;
 
 const SetBedtime = () => {
+  const { params } = useRoute<PropRoutes>();
   const navigator = useNavigation<ParentControlNavigatorProp>();
-  const [bedTimeMode, setBedtimeMode] = useState(false);
+
+  const {
+    isPending: isLoading,
+    error: fetcherror,
+    refetch,
+    data,
+  } = useGetKidById(params.childId);
+
+  if (isLoading) return <LoadingOverlay visible={isLoading} />;
+  const [bedTimeMode, setBedtimeMode] = useState(
+    data.isBedtimeEnabled ?? false
+  );
   const [bedtimeControls, setBedtimeControls] = useState({
     lockDuringBedtime: true,
     dimDuringBedtime: true,
@@ -26,18 +42,23 @@ const SetBedtime = () => {
     "everyday" | "weekdays" | "weekends"
   >("weekdays");
   const [schedule, setSchedule] = useState({
-    startTime: "9:00 PM",
-    stopTime: "07:00 AM",
+    startTime: data.bedtimeStart ?? "9:00 PM",
+    stopTime: data.bedtimeEnd ?? "07:00 AM",
   });
   const [isTimeModalOpen, setIsTimeModalOpen] = useState<
     "start" | "stop" | null
   >(null);
+  const { mutate, isPending, error } = useUpdateKids();
+  console.log("kids data", data);
+  if (fetcherror)
+    return <ErrorComponent refetch={refetch} message={fetcherror.message} />;
   return (
     <ScrollView
       className="flex-1"
       contentContainerClassName="min-h-full pb-10 bg-bgLight flex flex-col gap-y-10 sm:mx-auto max-w-screen-md w-full"
     >
       <PageTitle title="Bedtime Mode" goBack={() => navigator.goBack()} />
+      {error?.message && <ErrorMessageDisplay errorMessage={error?.message} />}
       <View className="p-5 mx-5  flex flex-col gap-y-2 rounded-2xl bg-white">
         <View className="flex flex-row items-center justify-between">
           <Text className="text-xl font-[abeezee] text- mb-2">
@@ -81,7 +102,7 @@ const SetBedtime = () => {
           <Text className="flex-1 font-[quilka]">Everyday</Text>
           <Switch
             disabled={bedTimeMode === false}
-            value={repeatDays === "everyday"}
+            value={repeatDays === "everyday" || data.bedtimeDays.includes[0]}
             onValueChange={() => setRepeatDays("everyday")}
           />
         </View>
@@ -101,13 +122,6 @@ const SetBedtime = () => {
             onValueChange={() => setRepeatDays("weekends")}
           />
         </View>
-        {/* <View className="flex flex-row justify-between items-center">
-          <Text className="flex-1 font-[quilka]">Custom</Text>
-          <Pressable onPress={() => setIsTimeModalOpen(true)}>
-            <Text>{"Select"}</Text>
-            <Icon name="ChevronRight" />
-          </Pressable>
-        </View> */}
       </View>
 
       <View
@@ -175,6 +189,18 @@ const SetBedtime = () => {
           />
         </View>
       </View>
+      <CustomButton
+        onPress={() =>
+          mutate({
+            id: params.childId,
+            isBedtimeEnabled: bedTimeMode,
+            bedtimeStart: schedule.startTime,
+            bedtimeEnd: schedule.stopTime,
+            bedtimeDays: [repeatDays],
+          })
+        }
+        text={isPending ? "Saving" : "Save changes"}
+      />
       <TimePickerOverlay
         visible={isTimeModalOpen === "start"}
         onClose={() => setIsTimeModalOpen(null)}
@@ -187,6 +213,7 @@ const SetBedtime = () => {
         onClose={() => setIsTimeModalOpen(null)}
         onConfirm={(string) => setSchedule((s) => ({ ...s, stopTime: string }))}
       />
+      <LoadingOverlay visible={isPending || isLoading} />
     </ScrollView>
   );
 };
