bad3530fabf58cbd223552b2f4477ade4190b74e
jutin0852 <jutindikonu8@gmail.com>
2025-11-27 10:57:45 +0100

    feat:kid profile Set

---
+++ screens/Kids/profile/ReadKidHistory.tsx
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function ReadKidHistory() {
+  return (
+    <View>
+      <Text>ReadKidHistory</Text>
+    </View>
+  )
+}
