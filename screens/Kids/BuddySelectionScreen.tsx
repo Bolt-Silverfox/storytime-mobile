9adc60074d40c464189a25150185f4e2a0372f31
MrELOWON <hillaryelowon@gmail.com>
2025-12-02 16:35:20 +0300

    Add BuddySelectionScreen feature

--- screens/Kids/BuddySelectionScreen.tsx
+++ screens/Kids/BuddySelectionScreen.tsx
@@ -1,6 +1,6 @@
 import { RouteProp, useNavigation, useRoute } from "@react-navigation/native";
 import { lazy, Suspense, useEffect, useState } from "react";
-import { ActivityIndicator, Image, Pressable, Text, View } from "react-native";
+import { ActivityIndicator, Pressable, Text, View } from "react-native";
 import ErrorComponent from "../../components/ErrorComponent";
 import Icon from "../../components/Icon";
 import LoadingOverlay from "../../components/LoadingOverlay";
@@ -10,27 +10,41 @@ import {
   KidsSetupProp,
 } from "../../Navigation/KidsSetupNavigator";
 import useSetStoryBuddy from "../../hooks/tanstack/mutationHooks/useSetStoryBuddy";
+
 const BuddySelectionComponent = lazy(
   () => import("../../components/BuddySelectionComponent")
 );
 
 type RouteProps = RouteProp<KidsSetupNavigatorParamList, "welcomeScreen">;
+
 const BuddySelectionScreen = () => {
   const { params } = useRoute<RouteProps>();
-  const [selected, setSelected] = useState<string>("");
   const navigator = useNavigation<KidsSetupProp>();
+
+  const [selected, setSelected] = useState<string>("");
+
   const { isPending, data, error, refetch } = useGetKidById(params.childId);
+
   const { mutate, isPending: isUpdating } = useSetStoryBuddy({
     kidId: params.childId,
     id: selected,
-    onSuccess: () =>
-      navigator.navigate("welcomeScreen", {
-        selected,
-        childId: params?.childId ?? "childIDisundefined",
-      }),
+    onSuccess: () => {
+     
+      navigator.getParent()?.navigate("index", {
+        childId: params.childId,
+      });
+    },
   });
 
+
   useEffect(() => {
+    if (data?.storyBuddyId) {
+      navigator.getParent()?.navigate("index", {
+        childId: params.childId,
+      });
+      return;
+    }
+
     setSelected(data?.storyBuddyId ?? "");
   }, [data]);
 
@@ -42,26 +56,25 @@ const BuddySelectionScreen = () => {
       <Text className="text-center font-[quilka] text-2xl">
         Hi, {data?.name ?? "User"}
       </Text>
-      <Text className="text-center font-[abeezee]">
-        Choose your Storytime Buddy
-      </Text>
+      <Text className="text-center font-[abeezee]">Choose your Storytime Buddy</Text>
+
       <Suspense fallback={<ActivityIndicator size={"large"} />}>
-        <BuddySelectionComponent
-          selected={selected}
-          setSelected={setSelected}
-        />
+        <BuddySelectionComponent selected={selected} setSelected={setSelected} />
       </Suspense>
 
       <Pressable
         disabled={!selected}
         onPress={() => mutate()}
-        className={`mx-5  flex flex-row justify-center items-center gap-x-4 sm:w-full py-4 rounded-full mt-4 max-w-screen-sm sm:mx-auto ${!selected ? "bg-purple/20" : "bg-purple"}`}
+        className={`mx-5 flex flex-row justify-center items-center gap-x-4 sm:w-full py-4 rounded-full mt-4 max-w-screen-sm sm:mx-auto ${
+          !selected ? "bg-purple/20" : "bg-purple"
+        }`}
       >
         <Text className="text-center text-white font-[quilka] text-2xl">
-          {"Continue"}
+          Continue
         </Text>
         <Icon name="ArrowRight" color="white" />
       </Pressable>
+
       <LoadingOverlay visible={isPending || isUpdating} />
     </View>
   );
