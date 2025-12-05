8d26b540c1d3cf4aeefc3ad036994517d2389511
Moluno-xiii <molunoprogress@gmail.com>
2025-12-05 01:58:14 +0100

    feat : added child generation screens

---
+++ screens/Kids/personalize/CustomizeKidsStoryPreviewScreen.tsx
@@ -0,0 +1,98 @@
+import { RouteProp, useNavigation, useRoute } from "@react-navigation/native";
+import {
+  Image,
+  ImageBackground,
+  Pressable,
+  ScrollView,
+  Text,
+  View,
+} from "react-native";
+import ChildButton from "../../../components/UI/ChildButton";
+import {
+  PersonalizeKidNavigatorParamList,
+  PersonalizeKidsNavigatorProps,
+} from "../../../Navigation/PersonalizeKidNavigator";
+import Icon from "../../../components/Icon";
+import colours from "../../../colours";
+
+type RoutePropTypes = RouteProp<
+  PersonalizeKidNavigatorParamList,
+  "previewScreen"
+>;
+
+const CustomizeKidsStoryPreviewScreen = () => {
+  const { params } = useRoute<RoutePropTypes>();
+  const navigator = useNavigation<PersonalizeKidsNavigatorProps>();
+  return (
+    <View className="flex flex-1 bg-bgLight pb-5">
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        className="flex-1"
+        contentContainerClassName="min-h-full"
+      >
+        <ImageBackground
+          source={require("../../../assets/images/story-generation-bg.png")}
+          resizeMode="cover"
+          className="flex-1"
+        >
+          <View className="flex flex-col items-center mx-4">
+            <Text className="text-center py-3 text-white font-[quilka] text-2xl">
+              Personalize
+            </Text>
+            <View className="bg-white border border-black/5 rounded-2xl mx-4 w-full mt-8 py-9 px-6">
+              <View className="bg-white px-4 rounded-2xl flex flex-col  py-4">
+                <Text className="text-text text-base font-[abeezee]">
+                  Hero's name
+                </Text>
+                <View className="relative flex flex-row items-center">
+                  <Text className="rounded-full flex-1 text-black  text-3xl font-[quilka]">
+                    Timmy
+                  </Text>
+                </View>
+              </View>
+              <View className="px-4 py-6 flex flex-col mb-12 gap-y-5 border border-black/10 rounded-2xl">
+                <Text className="text-text text-base font-[abeezee]">
+                  Story Theme
+                </Text>
+                <View className="bg-purple/20 border-4 border-purple rounded-2xl flex flex-col justify-center items-center py-9">
+                  <Image
+                    source={require("../../../assets/images/castle.png")}
+                    className="size-[100px]"
+                  />
+                </View>
+                <View className="bg-[#ECC607] rounded-2xl p-4 flex flex-row gap-x-6 items-center">
+                  <Icon name="OctagonAlert" />
+                  <Text className="font-[abeezee] text-xs flex-1">
+                    The hero name in every story will be automatically changed
+                    to {"Tim"}.
+                  </Text>
+                </View>
+              </View>
+              <Pressable
+                onPress={() => navigator.goBack()}
+                className="flex flex-row justify-center items-center gap-x-6"
+              >
+                <Text className="text-link font-[abeezee]">
+                  Change story theme
+                </Text>
+                <Icon name="Pencil" color={colours.link} />
+              </Pressable>
+            </View>
+          </View>
+        </ImageBackground>
+        <ChildButton
+          onPress={() =>
+            navigator.navigate("customizeStory", {
+              childId: params.childId,
+            })
+          }
+          disabled={false}
+          icon="ArrowRight"
+          text="See sample story"
+        />
+      </ScrollView>
+    </View>
+  );
+};
+
+export default CustomizeKidsStoryPreviewScreen;
