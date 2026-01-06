e6bb64e19f278ceaec85341f2e61a271b9502374
Moluno-xiii <molunoprogress@gmail.com>
2026-01-06 22:55:11 +0100

    feat : add parent favourites screen UI and data fetching

--- hooks/tanstack/queryHooks/queryParentFavourites.ts
+++ hooks/tanstack/queryHooks/queryParentFavourites.ts
@@ -1,7 +1,7 @@
 import { queryOptions } from "@tanstack/react-query";
 import apiFetch from "../../../apiFetch";
 import { BASE_URL } from "../../../constants";
-import { QueryResponse } from "../../../types";
+import { QueryResponse, Story } from "../../../types";
 import { getErrorMessage } from "../../../utils/utils";
 import useGetUserProfile from "./useGetUserProfile";
 
@@ -14,7 +14,7 @@ const queryParentsFavourites = () => {
         const request = await apiFetch(`${BASE_URL}/parent-favorites`, {
           method: "GET",
         });
-        const response: QueryResponse = await request.json();
+        const response: QueryResponse<Story[]> = await request.json();
         if (!response.success) throw new Error(response.message);
         return response;
       } catch (err) {
