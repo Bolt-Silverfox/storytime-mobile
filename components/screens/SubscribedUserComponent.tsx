053369a20fcbcf9951b35c1daa8023facf9be4fe
Moluno-xiii <molunoprogress@gmail.com>
2026-02-13 19:13:59 +0100

    fix : add PR #144 suggestions

--- components/screens/SubscribedUserComponent.tsx
+++ components/screens/SubscribedUserComponent.tsx
@@ -24,12 +24,12 @@ const SubscribedUserComponent = () => {
         <View className="mx-auto -mb-5 h-10 w-[90%] rounded-t-[32px] bg-white/60" />
         <View className="flex flex-col gap-y-10 rounded-t-[32px] bg-white px-4 py-5">
           <View className="flex flex-col gap-y-4">
-            <Text className="ont-[abeezee] text-base text-black">
+            <Text className="font-[abeezee] text-base text-black">
               Your plan
             </Text>
             <View className="flex h-[114px] flex-row items-center justify-between gap-x-3 rounded-[20px] border border-border-light px-6">
               <Text className="font-[abeezee] text-base text-text">
-                Monghly Plan
+                Monthly Plan
               </Text>
               <Text className="font-[quilka] text-[50px] text-[#333333]">
                 $2.99
