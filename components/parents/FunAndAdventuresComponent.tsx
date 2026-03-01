0cf717a09178aa21d298c2a9d06ae8efe208897c
TheShinobi <billmal071@gmail.com>
2026-03-01 17:57:20 +0100

    fix: use first available category instead of hardcoded name

--- components/parents/FunAndAdventuresComponent.tsx
+++ components/parents/FunAndAdventuresComponent.tsx
@@ -6,15 +6,11 @@ import queryStoryCategories from "../../hooks/tanstack/queryHooks/useGetsStoryCa
 import HomepageStoriesContainer from "../HomepageStoriesContainer";
 import HomeScreenCarouselComponent from "./HomeScreenCarouselComponent";
 
-const FUN_AND_ADVENTURE_NAME = "Fun and adventure";
-
 const FunAndAdventuresComponent = () => {
   const navigator = useNavigation<ParntHomeNavigatorProp>();
   const { data: categories } = useQuery(queryStoryCategories());
 
-  const category = categories?.find(
-    (c) => c.name.toLowerCase() === FUN_AND_ADVENTURE_NAME.toLowerCase()
-  );
+  const category = categories?.[0];
 
   const { data, isPending, error, refetch } = useQuery({
     ...queryGetStories({ category: category?.id }),
